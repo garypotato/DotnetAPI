54db101e01d201abff50ed5d1dcbaf8870275dd4
garypotato666@gmail.com <garypotato666@gmail.com>
2024-11-09 16:59:04 +1100

    user dapper to talk to database

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,6 @@
+using DotnetAPI.Data;
+using DotnetAPI.Dtos;
+using DotnetAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetAPI.Controllers;
@@ -13,18 +16,74 @@ public class UserController : ControllerBase
         _dapper = new DataContextDapper(config);
     }
 
-    [HttpGet("TestConnection")]
-    public DateTime TestConnection()
+    [HttpGet("users")]
+    public IEnumerable<User> GetUsers()
     {
-        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+        string sql = "select * from TutorialAppSchema.Users";
+        IEnumerable<User> users = _dapper.LoadData<User>(sql);
+
+        return users;
     }
 
-    [HttpGet("GetUsers/{testValue}")]
-    public string[] GetUsers(string testValue)
+    [HttpGet("users/{userId}")]
+    public User GetSingleUser(string userId)
     {
-        string[] responseArray = new string[] { "Hello", "World", testValue };
+        string sql = @"select * from TutorialAppSchema.Users 
+                        where userId = " + userId;
+        User user = _dapper.LoadDataSingle<User>(sql);
 
-        return responseArray;
+        return user;
+    }
+
+    [HttpPut("updateUser")]
+    public IActionResult UpdateUser(User user)
+    {
+        string sql = "update TutorialAppSchema.Users set FirstName = '"
+        + user.FirstName +
+        "', LastName = '" + user.LastName +
+        "', Email = '" + user.Email +
+        "', Gender = '" + user.Gender +
+        "', Active = '" + user.Active
+        + "' where UserId = " + user.UserId;
+
+        if (_dapper.ExecuteData(sql))
+        {
+            return Ok("User updated successfully");
+        }
+        else
+        {
+            return BadRequest("User update failed");
+        }
+    }
+
+    [HttpPost("createUser")]
+    public IActionResult CreateUser(UserToAddDto user)
+    {
+        string sql = "insert into TutorialAppSchema.Users (FirstName , LastName, Email, Gender, Active) values ('"
+        + user.FirstName + "', '" + user.LastName + "', '" + user.Email + "', '" + user.Gender + "', '" + user.Active + "')";
+        Console.WriteLine(sql);
+        if (_dapper.ExecuteData(sql))
+        {
+            return Ok("User created successfully");
+        }
+        else
+        {
+            return BadRequest("User creation failed");
+        }
+    }
+
+    [HttpDelete("deleteUser/{userId}")]
+    public IActionResult DeleteUser(string userId)
+    {
+        string sql = "delete from TutorialAppSchema.Users where UserId = " + userId;
+        if (_dapper.ExecuteData(sql))
+        {
+            return Ok("User deleted successfully");
+        }
+        else
+        {
+            return BadRequest("User deletion failed");
+        }
     }
 }
 
---
+++ Models/UserSalary .cs
@@ -0,0 +1,9 @@
+namespace DotnetAPI.Models
+{
+    public partial class UserSalary
+    {
+        public int UserId { get; set; }
+        public string Salary { get; set; } = "";
+        public string AvgSalary { get; set; } = "";
+    }
+}
